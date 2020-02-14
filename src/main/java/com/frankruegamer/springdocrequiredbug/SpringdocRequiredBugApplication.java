87d93fd0056231737f4dc66446cf2431f0306f0e
Frank Rügamer <frank.ruegamer@anevis-solutions.com>
2020-02-14 12:05:14 +0100

    Initial commit

---
+++ src/main/java/com/frankruegamer/springdocrequiredbug/SpringdocRequiredBugApplication.java
@@ -0,0 +1,13 @@
+package com.frankruegamer.springdocrequiredbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocRequiredBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdocRequiredBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankruegamer/springdocrequiredbug/SpringdocRequiredBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankruegamer.springdocrequiredbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocRequiredBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
